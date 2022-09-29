55be30ade6c87ee5c2df701a5cd332c8c2bdf975
anuLoveCoading <anusrivas07@gmail.com>
2022-09-30 00:46:18 +0530

    java class

--- java04.java
+++ java04.java
@@ -4,16 +4,16 @@
 public class java04 {
 
     public static void main(String[] args) {
-                                                                
-         System.out.println(60+120);
 
-         System.out.println("Anurag Mundra");
+         System.out.println (60+120) ;
 
-         System.out.println("Anurag");
+         System.out.println ("Anurag Mundra");
+
+         System.out.println ("Anurag");
 
          double  number = 1.2555;
 
-         System.out.println(number);
+         System.out.println (number);
 
 
     }

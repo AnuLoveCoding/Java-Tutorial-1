f974515310c78f1ce1d15878df5be3956c004c2c
anuLoveCoading <anusrivas07@gmail.com>
2022-09-29 18:08:18 +0530

    java04-class

--- java04.java
+++ java04.java
@@ -4,6 +4,17 @@
 public class java04 {
 
     public static void main(String[] args) {
+        
          System.out.println(60+120);
+
+         System.out.println("Anurag Mundra");
+
+         System.out.println("Anurag");
+
+         double  number = 1.2555;
+
+         System.out.println(number);
+
+
     }
 }

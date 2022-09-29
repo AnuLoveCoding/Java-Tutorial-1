6b0b36e552307471d0ac2b56df1b9fc4f3924855
anuLoveCoading <anusrivas07@gmail.com>
2022-09-29 14:37:32 +0530

    java01-class

---
+++ java01.java
@@ -0,0 +1,11 @@
+/**
+ * java01
+ */
+public class java01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(2+10);
+    }
+    
+}

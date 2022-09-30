501b479d85dce2aa7f45aa78fb47a7e7836e177d
anuLoveCoading <anusrivas07@gmail.com>
2022-09-30 16:18:03 +0530

    java-class

--- java01.java
+++ java01.java
@@ -5,15 +5,14 @@ import java. util. *;
 public class java01 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // System.out.println("Hello World");
         // System.out.println(2+10);
             // System.out.println(2+5);
 
-            System.out.println("anurag");
+            // System.out.println("anurag");
 
+            // What is the output of the Java code snippet?
 
-            
-
-    }
+              }
 
 }

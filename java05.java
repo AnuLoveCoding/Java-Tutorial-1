e7e2a49de1006e02a04ef4cd93bc5a2553158ca3
anuLoveCoading <anusrivas07@gmail.com>
2022-10-02 21:02:48 +0530

    java-class

--- java05.java
+++ java05.java
@@ -1,9 +1,17 @@
 /**
  * java05
  */
+import java.util.*;
 public class java05 {
 
     public static void main(String[] args) {
-        
+
+
+        System.out.println("Taking Input From-Java");
+
+        Scanner scn = new Scanner(System.in);
+
+
+        int A = scn.nextInt();
     }
 }

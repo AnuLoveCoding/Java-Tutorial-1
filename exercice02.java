8201ef215f2d4ad3162ec6872017a441812a97e0
anuLoveCoading <anusrivas07@gmail.com>
2022-10-02 23:05:39 +0530

    java - excercise

---
+++ exercice02.java
@@ -0,0 +1,21 @@
+/**
+ * exercice02
+ */
+import java.util.*;
+public class exercice02 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // Question  1 is Add three numbers;
+
+        int A =40;
+        int B =30;
+        int C = 50;
+        int sum = A+B+C;
+        System.out.println(sum);
+
+        
+        
+    }
+}

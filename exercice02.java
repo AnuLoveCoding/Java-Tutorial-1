d8ec3f6107c97fbe9937fd173c827d6a89e96ef3
anuLoveCoading <anusrivas07@gmail.com>
2022-10-02 23:45:08 +0530

    java-Project

--- exercice02.java
+++ exercice02.java
@@ -2,6 +2,8 @@
  * exercice02
  */
 import java.util.*;
+
+import javax.sound.midi.Soundbank;
 public class exercice02 {
 
     public static void main(String[] args) {
@@ -9,13 +11,46 @@ public class exercice02 {
 
         // Question  1 is Add three numbers;
 
-        int A =40;
-        int B =30;
-        int C = 50;
-        int sum = A+B+C;
-        System.out.println(sum);
+        // int A =40;
+        // int B =30;
+        // int C = 50;
+        // int sum = A+B+C;
+        // System.out.println(sum);
 
-        
+        // Question 2 is get CGPA ; count 5 subject marks;
+
+        // float subject1 = 95;
+        // float subject2 = 45;
+        // float subject3 = 63;
+        // float subject4 = 99;
+        // float subject5 = 66;
+        // to convert % into CGPA use 10/100 so in percentage divide by(100) or in CGPA divide by (10);
+        // System.out.println(subject1+subject2+subject3+subject4+subject5);
+
+    // float CGPA = (subject1 + subject2+ subject3 + subject4 + subject5)/50;  //for CGPA use;
+    //             System.out.print(CGPA + " CGPA ");
+    //             System.out.println("Congratulations:)");
+
+
+         //  Question 3, to ask userName and print name or print have a good day!.
+
+        //     System.out.println("Enter your name:");
+        //  String name = scn.nextLine();
+        //  System.out.println("Welcome "+name + ", Have a good day:)");  //use to add string use to (+)concatenation;
+
+        //  Qusetion 5, find user input number is integer yes or not.
+
+        System.out.println("Enter your number");
+
+        // int A = scn.hasNextInt();
+        // System.out.println(A);
+        System.out.println(scn.hasNextInt());
+
+
+
+
+
+
         
     }
 }
---
+++ java06.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+/**
+ * java06
+ */
+public class java06 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int A = scn.nextInt();
+        System.out.println(A);
+    }
+}

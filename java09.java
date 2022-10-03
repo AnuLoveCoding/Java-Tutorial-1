2c97d6bd00ece8d9ee9274f0da32c770cb01cd7b
anuLoveCoading <anusrivas07@gmail.com>
2022-10-03 10:34:21 +0530

    java-class

---
+++ java09.java
@@ -0,0 +1,47 @@
+/**
+ * java09
+ */
+import java.util.*;
+
+public class java09 {
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+
+        // Arthmatic perisedence && Asociativity operator;
+        // Asociativity(left to right);'
+        /* 
+              int a 6*5-34/2; //left to right(due to presidence);
+            // int a =  30-34/2;
+            //  int a = 30-17;
+            // 13 Ans;
+
+            int b = 60/5 - 34*2;
+            // int b = 12-34*2;
+            // int b = 12-68;
+            // int b= -56
+            //  -56 Ans;*/
+
+
+
+            // Quick quiz ;
+         /*   float  x = 5;
+            float  y = 1;
+        // left to right due to perisedence;
+            float k =  x * y/2;
+            System.out.println(k); */ 
+
+
+
+            int b = 1;
+            int c = 4;
+            int a = 5 ;
+            int k = (b*b) - (4*a*c)/(2*a);
+            System.out.println(k);
+
+
+
+
+    }
+}

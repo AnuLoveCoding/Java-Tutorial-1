86bfd03aa8e6c5f80b99fc7624417ceb8ac097d3
anuLoveCoading <anusrivas07@gmail.com>
2022-10-03 14:03:40 +0530

    java-class

---
+++ newexercise.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+/**
+ * newexercise
+ */
+public class newexercise {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+
+
+        
+
+        // Question 1, will be the result of the following expression;
+
+
+
+        // float a = 7/4f*9/2f;
+
+        // System.out.println(a);
+
+    // Question 2, write a program to encrypt a grade by adding 8 to it . Decrypt it to show the correct grade;
+
+
+        //  char grade = 'B';
+        //  grade = (char)(grade + 8);  //typecasting(char);
+
+        //  System.out.println(grade);
+
+        // //  Decrypting the grade;
+
+        // grade = (char)(grade-8);  //typecasting(char);
+        // System.out.println(grade);
+
+        // Question 3, the number is greater or not;
+        System.out.println("Enter Your Number : ");
+        int a = scn.nextInt();
+        
+        System.out.println(a>8);
+        
+
+    }
+}

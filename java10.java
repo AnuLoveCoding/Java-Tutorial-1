e44c9e116ce57d075a797b5c268d5b15cf8d70f2
anuLoveCoading <anusrivas07@gmail.com>
2022-10-03 13:25:06 +0530

    java-class

---
+++ java10.java
@@ -0,0 +1,40 @@
+/**
+ * java10
+ */
+import java.util.*;
+public class java10 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+       
+       
+        //  byte x = 5;
+        //  int y = 6;
+        //  short z =8;
+        //  int a = 164 + 4;
+        //  float b = (float)6.52 + x ;
+
+        //  System.out.println(b);
+
+        //  Increment and Decremeny;
+
+        int i = 7;
+        // 
+        System.out.println(++i);
+        System.out.println(i);
+        System.out.println(i++);
+        // System.out.println(i++);
+        System.out.println(i);
+
+        int j = 67;
+        int c = ++j;
+        System.out.println(c);
+
+
+
+
+
+
+
+    }
+}

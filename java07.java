e3ccd04fd99ab318e2ca71d0d15c2bbfbacd8b01
anuLoveCoading <anusrivas07@gmail.com>
2022-10-03 09:49:31 +0530

    Java-class

---
+++ java07.java
@@ -0,0 +1,27 @@
+/**
+ * java07
+ */
+import java.util.*;
+public class java07 {
+
+    public static void main(String[] args) {
+
+
+        Scanner scn = new Scanner(System.in);
+            System.out.println("Enter your number");
+        int A = scn.nextInt();
+        int  sum = A + 60;
+        sum *= 322;
+        System.out.println("The sum of Two number is "+ sum);
+        System.out.println("My result is : ");
+        System.out.println( 66666<sum);
+        System.out.println("true or false:");
+        System.out.println(64>8 && 64>5);
+
+
+
+
+
+    }
+
+}

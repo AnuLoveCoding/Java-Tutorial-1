2f3660f0478f09af93bc42e3090524db1cc763c0
anuLoveCoading <anusrivas07@gmail.com>
2022-10-04 11:41:27 +0530

    javaQuestion

--- java11.java
+++ java11.java
@@ -15,8 +15,8 @@ public class java11 {
         // System.out.println("Multiplication of Number");
         // System.out.println(a *2);
 
-    //     System.out.println("Enter Your Number");
-    //    int A = scn.nextInt();
+            //     System.out.println("Enter Your Number");
+            //    int A = scn.nextInt();
         // System.out.println("Table of number is");
         // for(int i=1;i<=A;i++){
         //     System.out.println(i*3);
@@ -25,12 +25,17 @@ public class java11 {
         // for (int i=1;i<=10;i++){
         //     System.out.println(i*5);
         // }
-    //   System.out.println("The addtion of 5 is");
-    //     for(int i=1;i<=A;i+=5){
-    //         System.out.println(i);
-    //     }
+            //   System.out.println("The addtion of 5 is");
+            //     for(int i=1;i<=A;i+=5){
+            //         System.out.println(i);
+            //     }
 
+                    // float a = (7*49/7+35/7);
+                    // System.out.println(a);
 
+                    // for (int i=10;i>=1;i-=2){
+                    //     System.out.println(i);
+                    // }
     
 
 

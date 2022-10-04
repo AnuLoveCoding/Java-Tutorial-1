47e9fc974b4e8f514fcb4f92de581d2a28356031
anuLoveCoading <anusrivas07@gmail.com>
2022-10-04 12:23:08 +0530

    javaString

--- javaString.java
+++ javaString.java
@@ -20,8 +20,8 @@ public class javaString {
 
             int a = 6;
            float b = 5.6454f;
-           System.out.printf("The value of a is %d and value of b is %f",a,b); // format-printing
-           System.out.format("The value of a is %d and value of b is %f",a,b);  //
+           System.out.printf("The value of a is %d and value of b is %.2f",a,b); // format-printing
+            //    System.out.format("The value of a is %d and value of b is %f",a,b);  //
 
 
 

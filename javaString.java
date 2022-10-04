af5246ea6e548e1fc178166b1e1514ad91389dc8
anuLoveCoading <anusrivas07@gmail.com>
2022-10-04 12:14:12 +0530

    java-project

--- javaString.java
+++ javaString.java
@@ -5,18 +5,23 @@ import java.util.*;
 public class javaString {
 
     public static void main(String[] args) {
-        
+
          Scanner scn = new Scanner(System.in);
 
-            System.out.println("Enter Your Name");
+            // System.out.println("Enter Your Name");
 
-            String a = scn.nextLine();
+            // String a = scn.nextLine();
 
-            System.out.print("Your Name is "+ a);
+            // System.out.print("Your Name is "+ a);
 
-            System.out.println(" : Congratulation");
+            // System.out.println(" : Congratulation");
 
+            // System.out.println();
 
+            int a = 6;
+           float b = 5.6454f;
+           System.out.printf("The value of a is %d and value of b is %f",a,b); // format-printing
+           System.out.format("The value of a is %d and value of b is %f",a,b);  //
 
 
 

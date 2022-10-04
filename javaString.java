2aeeaf6bbe06c7de3b282b17d1ff8abdee42a6eb
anuLoveCoading <anusrivas07@gmail.com>
2022-10-04 12:04:07 +0530

    java-project

---
+++ javaString.java
@@ -0,0 +1,45 @@
+/**
+ * javaString
+ */
+import java.util.*;
+public class javaString {
+
+    public static void main(String[] args) {
+        
+         Scanner scn = new Scanner(System.in);
+
+            System.out.println("Enter Your Name");
+
+            String a = scn.nextLine();
+
+            System.out.print("Your Name is "+ a);
+
+            System.out.println(" : Congratulation");
+
+
+
+
+
+
+            // String name = "Anurag";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

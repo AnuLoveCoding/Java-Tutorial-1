cbfcc2d48a262da2d7d61430a2b56cc3ce05366c
anuLoveCoading <anusrivas07@gmail.com>
2022-10-04 11:23:59 +0530

    java-question

---
+++ java11.java
@@ -0,0 +1,38 @@
+/**
+ * java11
+ */
+import java.util.*;
+
+
+public class java11 {
+
+    public static void main(String[] args) {
+         Scanner scn = new Scanner(System.in);
+
+
+        //  System.out.println("Enter Your Number ");
+        // int a = scn.nextInt();
+        // System.out.println("Multiplication of Number");
+        // System.out.println(a *2);
+
+    //     System.out.println("Enter Your Number");
+    //    int A = scn.nextInt();
+        // System.out.println("Table of number is");
+        // for(int i=1;i<=A;i++){
+        //     System.out.println(i*3);
+        // }
+
+        // for (int i=1;i<=10;i++){
+        //     System.out.println(i*5);
+        // }
+    //   System.out.println("The addtion of 5 is");
+    //     for(int i=1;i<=A;i+=5){
+    //         System.out.println(i);
+    //     }
+
+
+    
+
+
+    }
+}

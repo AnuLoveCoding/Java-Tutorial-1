5364f35d5ffc3a34855f248fa785448ef02e7c9a
anuLoveCoading <anusrivas07@gmail.com>
2022-10-04 13:13:41 +0530

    javaclass

---
+++ java16.java
@@ -0,0 +1,18 @@
+/**
+ * java16
+ */
+import java.util.*;
+public class java16 {
+
+    public static void main(String[] args) {
+        int age = 25;
+
+        if(age>18){
+            System.out.println("you can drive boy!");
+        }else{
+            System.out.println("you can't drive boy..)");
+        }
+
+        
+    }
+}

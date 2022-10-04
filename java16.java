c08959ffa9e24524b0fe34ce9a8ab00a079b82b3
anuLoveCoading <anusrivas07@gmail.com>
2022-10-04 13:21:00 +0530

    java-class

--- java16.java
+++ java16.java
@@ -5,14 +5,22 @@ import java.util.*;
 public class java16 {
 
     public static void main(String[] args) {
-        int age = 25;
-
+        int age = 25; //int age assign value(=) 25; not equal to 25;
+        // (1st Method)
         if(age>18){
             System.out.println("you can drive boy!");
         }else{
             System.out.println("you can't drive boy..)");
         }
 
-        
+            //(2nd) Method;
+        boolean cond = (age==18);
+
+        if(cond){
+            System.out.println("You can de=rive boy @");
+        }else{
+            System.out.println("You can't drive boy %");
+        }
+
     }
 }

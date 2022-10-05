66670fccaff23fdb4c116df4a5c7d9946a508bdb
anuLoveCoading <anusrivas07@gmail.com>
2022-10-05 13:21:35 +0530

    javaclass

--- java18.java
+++ java18.java
@@ -5,18 +5,205 @@ public class java18 {
 
 
 
-            System.out.println("Enter Your age : ");
-        int age = scn.nextInt();
+        //     System.out.println("Enter Your age : ");
+        // int age = scn.nextInt();
 
-        if(age>= 20){
-            System.out.println("You are experienced");
-        }else if(age>=18){
-            System.out.println("Your are semi experienced");
-        }else if(age>=10){
-            System.out.println("your are semi-semi experienced");
-        }else{
-            System.out.println("Your are not eligible");
-        }
+        // if(age>= 20){
+        //     System.out.println("You are experienced");
+        // }else if(age>=18){
+        //     System.out.println("Your are semi experienced");
+        // }else if(age>=10){
+        //     System.out.println("your are semi-semi experienced");
+        // }else{
+        //     System.out.println("Your are not eligible");
+        // }
+        // System.out.println("Enter Your age: ");
+
+
+
+            System.out.println("Enter Your Name:");
+            String name = scn.nextLine();
+
+            switch (name){  // not use of break;
+                case "Anurag" ->{
+                    System.out.println("good");
+                }
+                case "raj"->{
+                    System.out.println("Your are good boy!");
+                }
+                default ->{
+                    System.out.println("your are good>....");
+                }
+            }
+
+
+            // switch (age){
+            //     case 30 : System.out.println("You are not Adult");
+            //     break;//always use breaks in switch case;
+            //     case 25 : System.out.println("Your are adlut");
+            //     break;
+            //     case 21 : System.out.println("Your are teenager");
+            //     break;
+            //     case 15:System.out.println("Your are a baby");
+            //     break;
+            //     default :System.out.println("Your are small baby ");
+            // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }

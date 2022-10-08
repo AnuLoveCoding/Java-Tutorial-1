d18e0f252fb01f09ab3c7ba0a6788e0474370fd6
anuLoveCoading <anusrivas07@gmail.com>
2022-10-08 14:49:18 +0530

    whileLoop

--- whileLoop.java
+++ whileLoop.java
@@ -1,12 +1,30 @@
+
+import java.util.Scanner;
 public class whileLoop {
     public static void main(String[] args) {
         
+        Scanner sc =  new Scanner(System.in);
+
+
+        // print 1 to N;
+        System.out.println("Enter Your Number:");
+        // System.out.println("Here Your N");
+
+        int A = sc.nextInt();
+
+      int i = 1;
+
+      while(i<=A){
+
+        System.out.print(i+" ");
+
+        i++;
+      }
 
 
 
 
 
 
-        
     }
 }

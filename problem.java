c822290a0e4292ce2b5e53396d72a2eb8f782973
anuLoveCoading <anusrivas07@gmail.com>
2022-10-13 21:01:39 +0530

    problem class

--- problem.java
+++ problem.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
+
 /**
  * problem
  */
+import java.util.*;
 public class problem {
 
     public static void main(String[] args) {
-        
+         Scanner scn = new Scanner(System.in);
+
+         System.out.println("Enter Your Number: ");
+         int N = scn.nextInt();
+
+        //  int i=0;
+        int count = 0;
+         while(N>0){
+            
+                System.out.println("ANURAG");
+
+                count++;
+                
+            N--;
+         }
+
+         System.out.println(count);
+
+        //  
     }
 }
